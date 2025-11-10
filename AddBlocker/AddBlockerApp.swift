659e2529592203432fbaabbfa63f1879cadd8cc0
Shohjakhon Mamadaliev <shohjakhon.mamadaliev@ipratico.it>
2025-11-10 17:14:07 +0100

    Initial Commit

---
+++ AddBlocker/AddBlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddBlockerApp.swift
+//  AddBlocker
+//
+//  Created by Gabons on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AddBlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
